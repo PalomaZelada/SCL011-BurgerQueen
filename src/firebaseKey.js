93d48382a6f2e40751b69fd4ea1d6fb24d540401
PalomaZelada <p.zelada.m@gmail.com>
2019-12-13 11:09:19 -0300

    Actualizacion de carpetas

---
+++ src/firebaseKey.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBk25qezEHyn8sJJv4P1m_E9Xd6IAqq3Tw",
+    authDomain: "burger-queen-9b026.firebaseapp.com",
+    databaseURL: "https://burger-queen-9b026.firebaseio.com",
+    projectId: "burger-queen-9b026",
+    storageBucket: "burger-queen-9b026.appspot.com",
+    messagingSenderId: "917931296985",
+    appId: "1:917931296985:web:deba6b785896ff250dd6c8",
+    measurementId: "G-VNHPFKH3LV"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
+
+export default db;
