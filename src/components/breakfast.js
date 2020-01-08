245a2dcaaecbd38e3a14902dbc3318d2fd31587c
PalomaZelada <53873028+PalomaZelada@users.noreply.github.com>
2020-01-08 17:59:44 -0300

    Adding styles to buttons and order (#6)

--- src/components/breakfast.js
+++ src/components/breakfast.js
@@ -3,10 +3,12 @@ import BasicButton from './basicButton';
 import data1 from '../data/data.json';
 import db from '../data/firebaseKey';
 import Client from './infoClient';
+import Button from 'react-bootstrap/Button'
+
 class Breakfast extends Component {
   constructor(props) {
     super(props);
-  //Se establecen los estados 
+    //Se establecen los estados 
     this.state = {
       data: data1.breakfast,
       item: [],
@@ -18,93 +20,93 @@ class Breakfast extends Component {
   //Funcion para añadir items al pedido
   addingItem = order => {
     let orderedItems = this.state.item;
-  //Aqui utilizamos push para meter al array el objeto del menu que seleccionemos
+    //Aqui utilizamos push para meter al array el objeto del menu que seleccionemos
     orderedItems.push(order)
-  //Se actualiza el estado de item que ahora esta guardando los items del pedido
+    //Se actualiza el estado de item que ahora esta guardando los items del pedido
     this.setState({
       item: orderedItems,
     });
-  //Aqui se suma el valor de cada producto al total, por eso se establece el 0  
-  let orderTotal = this.state.item.reduce( (acc, elem) => acc + elem.price, 0)
-  //Actualizamos el estado de price que ahora guarda el total del pedido
-  this.setState({
-    price: orderTotal
-  })
-  }  
+    //Aqui se suma el valor de cada producto al total, por eso se establece el 0  
+    let orderTotal = this.state.item.reduce((acc, elem) => acc + elem.price, 0)
+    //Actualizamos el estado de price que ahora guarda el total del pedido
+    this.setState({
+      price: orderTotal
+    })
+  }
   //Funcion para guardar input del cliente
   handleInputChange = event => {
     this.setState({
-      [event.name] :event.value
+      [event.name]: event.value
     })
-    }
+  }
 
   //Funcion para eliminar items del pedido y restar el valor del total
-  deletingItem(id, price){
+  deletingItem(id, price) {
     this.setState({
       item: this.state.item.filter((elem, i) => {
-  //Cuando encuentra el id de un producto lo "filtra" del arreglo eso significa que lo elimina
+        //Cuando encuentra el id de un producto lo "filtra" del arreglo eso significa que lo elimina
         return i !== id
       })
     })
-  //Aqui guardamos en variables los valores de los items y los items del pedido y despues las restamos
+    //Aqui guardamos en variables los valores de los items y los items del pedido y despues las restamos
     let stateOrder = this.state.price
     let orderPrice = price
     let finalOrder = stateOrder - orderPrice
-  //Se actualiza el estado de precio despues de que se resten los valores
+    //Se actualiza el estado de precio despues de que se resten los valores
     this.setState({
       price: finalOrder
     })
-    }
+  }
   //Aqui guardamos los pedidos en firebase
   savingOrders = () => {
     //Guardo en variables los estados actuales del array item y precio
-      let newOrder= this.state.item
-      let newTotal= this.state.price
-      let newClient= this.state.client
-      let newTable= this.state.table
-      db.collection("orders").add({
-        clientName:newClient,
-        table:newTable,
-        orderedItems: newOrder,
-        price: newTotal
+    let newOrder = this.state.item
+    let newTotal = this.state.price
+    let newClient = this.state.client
+    let newTable = this.state.table
+    db.collection("orders").add({
+      clientName: newClient,
+      table: newTable,
+      orderedItems: newOrder,
+      price: newTotal
+    })
+
+      .then(function (docRef) {
+        console.log("Document written with ID: ", docRef.id);
+        //Cuando se guarda el pedido se muestra el mensaje que fue enviado a cocina
+        alert("Pedido enviado a cocina")
       })
-      
-        .then(function (docRef) {
-          console.log("Document written with ID: ", docRef.id);
-    //Cuando se guarda el pedido se muestra el mensaje que fue enviado a cocina
-          alert("Pedido enviado a cocina")
-        })
-    //Aqui limpio item y price para inicia un pedido nuevo
-        .then(()=>{
-        let emptyOrder= []
-        let emptyTotal= 0
-        let emptyInput= ''
-        let emptyTable=''
+      //Aqui limpio item y price para inicia un pedido nuevo
+      .then(() => {
+        let emptyOrder = []
+        let emptyTotal = 0
+        let emptyInput = ''
+        let emptyTable = ''
         this.setState({
-          item:emptyOrder,
-          price:emptyTotal,
-          client:emptyInput,
-          table:emptyTable
-          })
+          item: emptyOrder,
+          price: emptyTotal,
+          client: emptyInput,
+          table: emptyTable
         })
-        .catch(function (error) {
-          console.error("Error adding document: ", error);
-          alert("Hubo un error en el envio")
-        });   
-    }
+      })
+      .catch(function (error) {
+        console.error("Error adding document: ", error);
+        alert("Hubo un error en el envio")
+      });
+  }
 
   render() {
     const dataBreakfast = this.state.data.map((data, i) => {
       return (
-  //A BasicButton le damos la funcion addingItem on click y con los props muestra el nombre y precio de los productos
+        //A BasicButton le damos la funcion addingItem on click y con los props muestra el nombre y precio de los productos
         <div>
-        <BasicButton
-          clicking={this.addingItem}
-          products={data}
-          key={data.id}
-          name={data.item} 
-          price={data.price}
-        /> 
+          <BasicButton
+            clicking={this.addingItem}
+            products={data}
+            key={data.id}
+            name={data.item}
+            price={data.price}
+          />
         </div>
       )
     }
@@ -112,36 +114,38 @@ class Breakfast extends Component {
 
     return (
       <div>
-        <Client 
-        change={event=>this.handleInputChange(event.target)}
+        <Client
+          change={event => this.handleInputChange(event.target)}
         />
         {dataBreakfast}
-        <p>Mesa n° {this.state.table}- Nombre: {this.state.client}</p>
-        {this.state.item.map((order, i)=>{
+        <p className="inputLocation2">Mesa n° {this.state.table}- Nombre: {this.state.client}</p>
+        {this.state.item.map((order, i) => {
           return (
-            <div>
-               <p>
+            <div className="boxStyle2">
+              <p>
                 {order.item} ${order.price}
-              <span>
-              <button
-              onClick={this.deletingItem.bind(this, i, order.price)}
-              >
-                Eliminar
-              </button>    
-              </span>
+                <span>
+                  <Button
+                    variant="danger"
+                    onClick={this.deletingItem.bind(this, i, order.price)}>
+                    Eliminar
+              </Button>
+                </span>
               </p>
             </div>
           )
         })
         }
-        <p>
-         $ {this.state.price}
+        <p className="totalStyle2">
+          $ {this.state.price}
         </p>
         <div>
-        <button 
-        onClick={()=>this.savingOrders()}>
-          Enviar
-        </button> 
+          <Button
+            className="inputLocation2"
+            onClick={() => this.savingOrders()}
+            variant="primary">
+            Enviar
+        </Button>
         </div>
       </div>
     )
